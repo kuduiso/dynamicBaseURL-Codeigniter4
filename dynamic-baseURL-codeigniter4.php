37b8cb8087d0b9e54fcdf1c209978b7f791de500
kuduiso <49147945+kuduiso@users.noreply.github.com>
2020-11-28 23:40:14 +0700

    Update dynamic-baseURL-codeigniter4.php

--- dynamic-baseURL-codeigniter4.php
+++ dynamic-baseURL-codeigniter4.php
@@ -2,4 +2,4 @@
 
 $base = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']) : 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']); 
 
-defined('BASE') 				|| define('BASE', $base); //dynamically baseURL
+defined('BASE') || define('BASE', $base); //dynamically baseURL
